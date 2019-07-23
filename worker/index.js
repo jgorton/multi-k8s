fe7b0ad679ffd368a163ce72897f52b89d74876b
jgorton <jared.gorton@zirous.com>
2019-07-23 13:24:11 -0500

    changed fib calculator process

--- worker/index.js
+++ worker/index.js
@@ -10,7 +10,16 @@ const sub = redisClient.duplicate();
 
 function fib(index) {
   if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+  //  return fib(index - 1) + fib(index - 2);
+    var v1, v2, sum, ctr;
+    v1 = 1;
+    v2 = 1;
+    for (ctr = 1; ctr < index; ctr++) {
+	if (ctr % 2 == 1) v1 += v2;
+	else v2 += v1;
+    }
+    sum = v1 > v2 ? v1 : v2;
+    return sum;
 }
 
 sub.on('message', (channel, message) => {
